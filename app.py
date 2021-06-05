4c47e3ca856b9b3385798a8cfa5bff7c7c9d5c50
Vivek M R <vivekmrofficial@gmail.com>
2021-06-05 11:54:06 +0530

    Initial app

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method=="POST":
+        print("File Recieved")
+    return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(debug=True,threaded=True)
